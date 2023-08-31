3132f532baebdd063c984934268435f62b5e9d25
OrcinoNeto <129555010+OrcinoNeto@users.noreply.github.com>
2023-08-31 18:07:19 -0300

    Update index.js
    Add input para pedir qnt de fotos

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const botaoFetch = window.document.getElementById('botaoFetch')
 const resultado = document.getElementById('resultado')
+const numberInput = document.getElementById('numberInput')
 
 botaoFetch.addEventListener('click', ()=>{
-    fetch("https://dog.ceo/api/breeds/image/random/4")
+    fetch("https://dog.ceo/api/breeds/image/random/" + numberInput.value)
     .then(res =>{return res.json()})
     // .then(data => resultado.innerHTML = `<img src="${data.message}"/>`)
-    .then(data => data.message.map((foto)=> {
+    .then(data =>{        
+        data.message.map((foto)=> {
         resultado.innerHTML += `<img src=${foto} />`
-    }) )
+    })
+    }  )
     .catch(error => resultado.innerHTML = `<h1>Erro</h1>`)
 
 })
-
-
-
-
